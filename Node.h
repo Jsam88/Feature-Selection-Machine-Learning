37c678529309b67c96c5143d614900c9313d01cd
Jsam88 <jordansam@sbcglobal.net>
2022-05-15 22:29:58 -0700

    Finished main and Node class Part 1

---
+++ Node.h
@@ -0,0 +1,57 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#ifndef NODE_H
+#define NODE_H
+
+class Features {
+    public:
+        int data;
+        Features(int data) {
+        this -> data = data;
+        }
+};
+
+class Node {
+    public:
+        vector<Node*> child_nodes;
+        vector<Features*> user_features;
+        double feature_accuracy;
+    
+    double feature_evaluator(){                                 //Part 1: Need to utilize dummy evaluator that produces a random value as of now
+        double random_val = rand() % 100.0;
+        return random_val;
+    }
+
+    Node() {                                                    //Provide the accuracy of the starting node if there are no features
+        feature_accuracy = feature_evaluator();
+    }
+
+    void pushback_feature(Features* new_feature){               //Push back new feature onto the list of user features
+        this->user_features.push_back(new_feature);
+    }
+
+    void pushback_child(Node* new_child){                       //Push back new child node onto the list of child nodes
+        this->child_nodes.push_back(new_child);
+    }
+
+    Node(vector<Features*> user_features){                      //Return accuracy of some combination of features
+        for (int i = 0; i < user_features.size(); i++){
+            this->user_features.push_back(user_features.at(i));
+        }
+        feature_accuracy = feature_evaluator();
+    }
+
+    void print_features(){                                      //helper function to print out current features
+        int num_features;
+        cout << "{ ";
+        for (int i = 0; i < user_features.size(); i++){         //Get # of features in vector
+            cout << user_features.at(i)->data << " ";
+        }
+        cout << "}";
+    }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,43 @@
 //Part I: Implementing the greedy search algorithms only (No need to implement the classifier or the leave-one-out validator). As input, you only need total number of features (not the data file)
+#include "Node.h"
+#include "GreedySearch.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+int num_features;
+int user_input;
+vector<Features*> features_list;
+
+cout << "Welcome to Jordan Sam's Feature Selection Algorithm." << endl;
+cout << "Please enter total number of features: ";
+cin >> num_features;
+
+    while(num_features > 0){
+        features_list.push_back(num_features);
+        num_features--;
+    }
+
+cout << "Type the number of the algorithm you want to run." << endl << "1) Forward Selection" << endl << "2) Backward Elimination" << endl;
+cin >> user_input;
+
+cout << "Using no features and 'random' evaluation, I get an accuracy of xx.x%" << endl;
+
+if(userInput == 1){
+    greedy_search->forward_selection(features_list);
+}
+
+else if(userInput == 2){
+    greedy_search -> backward_elimination(features_list)
+}
+
+else {
+    cout << "No algorithm selected. Please try again." << endl;
+    return -1;
+}
+
+    return 0;
+}
