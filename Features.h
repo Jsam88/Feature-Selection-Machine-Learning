b2519a67facd4c5f8997ea0697a28dddc5532a25
Jsam88 <jordansam@sbcglobal.net>
2022-05-30 13:41:26 -0700

    Added features.h to get future data

---
+++ Features.h
@@ -0,0 +1,15 @@
+#ifndef FEATURE_H
+#define FEATURE_H
+
+using namespace std;
+
+class Features {
+    public:
+        int data;
+
+    public:
+        Features(int data) {
+        this -> data = data;
+        }
+};
+#endif
